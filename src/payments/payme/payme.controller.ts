5ad9938fd96aa8cd1f58ba6f9e6651d350a0261e
bek <shoyatbekov03032003@gmail.com>
2024-05-01 17:47:43 +0500

    payme 1.0.1 basic auth added

--- src/payments/payme/payme.controller.ts
+++ src/payments/payme/payme.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { PaymeService } from './payme.service';
 import { RequestBody } from './types/incoming-request-body';
+import { BasicAuthGuardPayme } from 'src/auth/basic-auth-payme.guard';
 
 @Controller('payme')
 export class PaymeController {
   constructor(private readonly paymeService: PaymeService) {}
   @Post()
+  @UseGuards(BasicAuthGuardPayme)
   async handleTransactionMethods(@Body() reqBody: RequestBody) {
     const result = await this.paymeService.handleTransactionMethods(reqBody);
     console.log('response', result);
