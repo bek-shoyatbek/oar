df4d0191b8bab92ebb1090cbff8eba84d2f0faa0
bek <shoyatbekov03032003@gmail.com>
2024-05-01 18:57:44 +0500

    payme 1.0.4 auth validation response added

--- src/auth/guards/payme.guard.ts
+++ src/auth/guards/payme.guard.ts
@@ -5,22 +5,31 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class PaymeBasicAuthGuard implements CanActivate {
   constructor(private configService: ConfigService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
+    const request = context.switchToHttp().getRequest<Request>();
+    const response = context.switchToHttp().getResponse<Response>();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
       throw new UnauthorizedException("Token doesn't exist");
     }
     try {
       const decoded = this.decodeToken(token);
-      if (!decoded)
-        throw new UnauthorizedException("Basic token doesn't exist");
+      if (!decoded) {
+        response.status(200).send({
+          id: request.body.id,
+          error: {
+            code: -32504,
+            message: 'Insufficient privileges to perform this operation',
+          },
+        });
+        return;
+      }
 
       console.log('decoded', decoded);
       const [username, password] = decoded.split(':');
@@ -35,10 +44,24 @@ export class PaymeBasicAuthGuard implements CanActivate {
         this.configService.get<string>('PAYME_PASSWORD_TEST') === password;
 
       if (!isValidUsername || !isValidPassword) {
-        throw new UnauthorizedException();
+        response.status(200).send({
+          id: request.body.id,
+          error: {
+            code: -32504,
+            message: 'Insufficient privileges to perform this operation',
+          },
+        });
+        return;
       }
     } catch {
-      throw new UnauthorizedException();
+      response.status(200).send({
+        id: request.body.id,
+        error: {
+          code: -32504,
+          message: 'Insufficient privileges to perform this operation',
+        },
+      });
+      return;
     }
     return true;
   }
