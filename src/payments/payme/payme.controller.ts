33bd63cf00dc140af563d8d74e5e947a3750613e
bek <shoyatbekov03032003@gmail.com>
2024-05-03 11:20:16 +0500

    payme logs added

--- src/payments/payme/payme.controller.ts
+++ src/payments/payme/payme.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { PaymeService } from './payme.service';
 import { RequestBody } from './types/incoming-request-body';
 import { PaymeBasicAuthGuard } from 'src/auth/guards/payme.guard';
+import { log } from 'console';
 
 @Controller('payme')
 export class PaymeController {
@@ -9,6 +10,7 @@ export class PaymeController {
   @Post()
   @UseGuards(PaymeBasicAuthGuard)
   async handleTransactionMethods(@Body() reqBody: RequestBody) {
+    log('payme:reqBody', reqBody);
     const result = await this.paymeService.handleTransactionMethods(reqBody);
     console.log('response', result);
     return result;
