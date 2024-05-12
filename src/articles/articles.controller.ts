3d1947c4ad8b09e36395b8add76827651985dac6
bek-shoyatbekov <shoyatbekov03032003@gmail.com>
2024-05-12 22:53:28 +0500

    updating article by id fixed

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -11,58 +11,68 @@ import {
   Query,
   UploadedFiles,
   UseFilters,
-  UseInterceptors
-} from "@nestjs/common";
-import { ArticlesService } from "./articles.service";
-import { FileFieldsInterceptor } from "@nestjs/platform-express";
-import { Prisma } from "@prisma/client";
-import { S3Service } from "src/aws/s3/s3.service";
-import { PrismaClientExceptionFilter } from "src/exception-filters/prisma/prisma.filter";
-import { STORAGE } from "../constants/storage";
+  UseInterceptors,
+} from '@nestjs/common';
+import { ArticlesService } from './articles.service';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { Prisma } from '@prisma/client';
+import { S3Service } from 'src/aws/s3/s3.service';
+import { PrismaClientExceptionFilter } from 'src/exception-filters/prisma/prisma.filter';
+import { STORAGE } from '../constants/storage';
+import { info } from 'node:console';
 
-@Controller("articles")
+@Controller('articles')
 export class ArticlesController {
   constructor(
     private readonly articlesService: ArticlesService,
-    private readonly s3Service: S3Service
-  ) {
-  }
+    private readonly s3Service: S3Service,
+  ) {}
 
-  @Post("create")
-  @UseInterceptors(FileFieldsInterceptor(
-    [
-      { name: "articleImage", maxCount: 1 },
-      {
-        name: "bannerImageWeb",
-        maxCount: 1
-      }, {
-      name: "bannerImageMobile",
-      maxCount: 1
-    }
-    ],
-    { storage: STORAGE }
-  ))
+  @Post('create')
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'articleImage', maxCount: 1 },
+        {
+          name: 'bannerImageWeb',
+          maxCount: 1,
+        },
+        {
+          name: 'bannerImageMobile',
+          maxCount: 1,
+        },
+      ],
+      { storage: STORAGE },
+    ),
+  )
   @UseFilters(PrismaClientExceptionFilter)
   async create(
-    @UploadedFiles() files: {
+    @UploadedFiles()
+    files: {
       articleImage: Express.Multer.File[];
       bannerImageWeb: Express.Multer.File[];
       bannerImageMobile: Express.Multer.File[];
     },
-    @Body() createArticleDto: Prisma.ArticlesCreateInput
+    @Body() createArticleDto: Prisma.ArticlesCreateInput,
   ) {
-
     const articleImage = files.articleImage[0];
     const bannerImageWeb = files.bannerImageWeb[0];
     const bannerImageMobile = files.bannerImageMobile[0];
 
     if (!articleImage || !bannerImageMobile || !bannerImageWeb) {
-      throw new BadRequestException("articleImage,bannerImageWeb and mobile are required");
+      throw new BadRequestException(
+        'articleImage,bannerImageWeb and mobile are required',
+      );
     }
 
-    const filesToUpload = [this.s3Service.upload(articleImage), this.s3Service.upload(bannerImageWeb), this.s3Service.upload(bannerImageMobile)];
+    const filesToUpload = [
+      this.s3Service.upload(articleImage),
+      this.s3Service.upload(bannerImageWeb),
+      this.s3Service.upload(bannerImageMobile),
+    ];
 
-    const [articleImageURL, bannerImageWebURL, bannerImageMobileURL] = await Promise.all(filesToUpload);
+    const [articleImageURL, bannerImageWebURL, bannerImageMobileURL] =
+      await Promise.all(filesToUpload);
 
     createArticleDto.articleImage = articleImageURL;
 
@@ -73,34 +83,39 @@ export class ArticlesController {
     return await this.articlesService.create(createArticleDto);
   }
 
-  @Patch("update/:id")
+  @Patch('update/:id')
   @UseFilters(PrismaClientExceptionFilter)
-  @UseInterceptors(FileFieldsInterceptor(
-    [
-      { name: "articleImage", maxCount: 1 },
-      {
-        name: "bannerImageWeb",
-        maxCount: 1
-      }, {
-      name: "bannerImageMobile",
-      maxCount: 1
-    }
-    ],
-    { storage: STORAGE }
-  ))
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'articleImage', maxCount: 1 },
+        {
+          name: 'bannerImageWeb',
+          maxCount: 1,
+        },
+        {
+          name: 'bannerImageMobile',
+          maxCount: 1,
+        },
+      ],
+      { storage: STORAGE },
+    ),
+  )
   async update(
     @Body() updateArticleDto: Prisma.ArticlesUpdateInput,
-    @UploadedFiles() files: {
+    @UploadedFiles()
+    files: {
       articleImage?: Express.Multer.File[];
       bannerImageWeb?: Express.Multer.File[];
       bannerImageMobile?: Express.Multer.File[];
     },
-    @Param("id") id: string
+    @Param('id') id: string,
   ) {
-
     const articleImage = files?.articleImage[0];
-    const bannerImageWeb = files?.bannerImageWeb[0];
-    const bannerImageMobile = files?.bannerImageMobile[0];
+    const bannerImageWeb = files.bannerImageWeb && files.bannerImageWeb[0];
+    const bannerImageMobile =
+      files.bannerImageMobile && files.bannerImageMobile[0];
+
 
     if (articleImage) {
       updateArticleDto.articleImage = await this.s3Service.upload(articleImage);
@@ -111,30 +126,31 @@ export class ArticlesController {
     }
 
     if (bannerImageMobile) {
-      updateArticleDto.imageMobile = await this.s3Service.upload(bannerImageMobile);
+      updateArticleDto.imageMobile =
+        await this.s3Service.upload(bannerImageMobile);
     }
 
     return await this.articlesService.update(id, updateArticleDto);
   }
 
-  @Get("all")
+  @Get('all')
   @UseFilters(PrismaClientExceptionFilter)
   async findAll(
-    @Query("isPublished", new ParseBoolPipe({ optional: true }))
-      isPublished: boolean
+    @Query('isPublished', new ParseBoolPipe({ optional: true }))
+    isPublished: boolean,
   ) {
     return await this.articlesService.findAll(isPublished);
   }
 
-  @Get("single/:id")
+  @Get('single/:id')
   @UseFilters(PrismaClientExceptionFilter)
-  async findOne(@Param("id") id: string) {
+  async findOne(@Param('id') id: string) {
     return await this.articlesService.findOne(id);
   }
 
-  @Delete("remove/:id")
+  @Delete('remove/:id')
   @UseFilters(PrismaClientExceptionFilter)
-  async remove(@Param("id") id: string) {
+  async remove(@Param('id') id: string) {
     return await this.articlesService.remove(id);
   }
 }
