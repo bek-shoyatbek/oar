7eda67077d1aaab11e563dcabc5711d1d0d2dc0f
bek-shoyatbekov <shoyatbekov03032003@gmail.com>
2024-05-15 23:03:03 +0500

    Lessons updating problem 500 fixed

--- src/lessons/lessons.controller.ts
+++ src/lessons/lessons.controller.ts
@@ -94,16 +94,13 @@ export class LessonsController {
       attachedFiles?: Express.Multer.File[];
     },
   ) {
-    const video = files?.video[0];
-    const attachedFiles = files?.attachedFiles;
-
-    if (video) {
-      updateLessonDto.video = await this.s3Service.upload(video);
+    if (files?.video) {
+      updateLessonDto.video = await this.s3Service.upload(files.video[0]);
     }
 
-    if (attachedFiles) {
+    if (files?.attachedFiles) {
       updateLessonDto.attachedFiles = await Promise.all([
-        ...attachedFiles?.map((file) => this.s3Service.upload(file)),
+        ...files?.attachedFiles?.map((file) => this.s3Service.upload(file)),
       ]);
     }
 
