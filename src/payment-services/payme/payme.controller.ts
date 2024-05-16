ba2978ed76bded254fa6c83b235a763991398ed5
bek <shoyatbekov03032003@gmail.com>
2024-05-16 11:44:10 +0500

    payment services finished

--- src/payment-services/payme/payme.controller.ts
+++ src/payment-services/payme/payme.controller.ts
@@ -9,7 +9,6 @@ import {
 import { PaymeService } from './payme.service';
 import { RequestBody } from './types/incoming-request-body';
 import { PaymeBasicAuthGuard } from 'src/auth/guards/payme.guard';
-import { log } from 'console';
 
 @Controller('payme')
 export class PaymeController {
@@ -19,9 +18,6 @@ export class PaymeController {
   @UseGuards(PaymeBasicAuthGuard)
   @HttpCode(HttpStatus.OK)
   async handleTransactionMethods(@Body() reqBody: RequestBody) {
-    log('payme:reqBody', reqBody);
-    const result = await this.paymeService.handleTransactionMethods(reqBody);
-    console.log('response', result);
-    return result;
+    return await this.paymeService.handleTransactionMethods(reqBody);
   }
 }
