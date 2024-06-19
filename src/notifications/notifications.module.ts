6ed916e3ddd99e36e9fb04a6ec6f19e00a5591ce
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-06-20 00:16:53 +0500

    non-nullable articleImageWeb and Mobile fixed

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
+import { MailModule } from 'src/mail/mail.module';
+import { SmsModule } from 'src/sms/sms.module';
 
 @Module({
-  controllers: [],
+  imports: [MailModule, SmsModule],
   providers: [NotificationsService],
 })
 export class NotificationsModule {}
--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -3,13 +3,5 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class NotificationsService {
   constructor() {}
-
-  async sendMail() {
-    return 'hello';
-  }
-
-  async sendSms() {
-    return 'hello';
-  }
-  
+    
 }
