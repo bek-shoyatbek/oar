79a9a665f71991b37f43f5c9c81995c973f307db
bek <shoyatbekov03032003@gmail.com>
2024-06-19 15:05:11 +0500

    statics:test 1.0.1

--- src/statics/statics.service.ts
+++ src/statics/statics.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
+import { ObjectId } from 'mongodb';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
@@ -15,14 +16,24 @@ export class StaticsService {
   }
 
   async getById(id: string) {
-    return await this.prismaService.statics.findUnique({ where: { id } });
+    this.isValidObjectId(id);
+    const file = await this.prismaService.statics.findUnique({ where: { id } });
+    if (!file) {
+      throw new BadRequestException('File not found');
+    }
+    return file;
   }
 
   async getByIdx(idx: string) {
-    return await this.prismaService.statics.findMany({ where: { idx } });
+    const files = await this.prismaService.statics.findMany({ where: { idx } });
+    if (!files) {
+      throw new BadRequestException('File not found');
+    }
+    return files;
   }
 
   async update(id: string, updateDto: Prisma.StaticsUpdateInput) {
+    this.isValidObjectId(id);
     return await this.prismaService.statics.update({
       where: { id },
       data: updateDto,
@@ -30,6 +41,13 @@ export class StaticsService {
   }
 
   async remove(id: string) {
+    this.isValidObjectId(id);
     return await this.prismaService.statics.delete({ where: { id } });
+  }
+
+  isValidObjectId(id: string) {
+    if (!ObjectId.isValid(id)) {
+      throw new BadRequestException('Invalid id');
+    }
   }
 }
